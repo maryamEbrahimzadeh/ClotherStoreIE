151c7e932e0ffd2dd5411e40a218913251f624d5
maryam ebrahimzadeh <maryam.ebrahimzadeh1997@yahoo.com>
2018-06-14 10:56:39 +0430

    for multiple layout for login

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-      {{$store.getters.getis_login}}
+      
     </div>
 </template>
 
@@ -12,7 +12,7 @@ export default {
     //layout : 'logedIn',
     middleware : 'login'
     ,
-    
+    // {{$store.getters.getis_login}}
     // methods: {
     //   set_layout : function(){
     //     if($store.getters.getis_login){
