6511ef6700b76f1ea52a13e507e07927930c9fc6
maryam ebrahimzadeh <maryam.ebrahimzadeh1997@yahoo.com>
2018-06-15 20:28:48 +0430

    a little for new layout

---
+++ components/MainNavBarLogin.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="navbar">
+        <div class="userData">
+            <user-info :username="query" />
+        </div>
+        <div class="title">
+            Clothster
+        </div>
+        <div class="searchbar">
+            <search-bar @searched="searchHandler" />
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <div class="categories">
+            <categories-bar />
+        </div>
+        
+    </div>
+</template>
+
+
+<script>
+import SearchBar from '~/components/SearchBar';
+import UserInfo from '~/components/UserInfo';
+import CategoriesBar from '~/components/CategoriesBar';
+export default {
+    components: {
+        SearchBar,
+        UserInfo,
+        CategoriesBar
+    },
+    data() {
+        return {
+            query: ''
+        }
+    },
+    methods: {
+        searchHandler: function(e) {
+            this.query = e;
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .navbar {
+        height: 10vh;
+        width: 100%;
+        padding: 1rem;
+        display: flex;
+        /* border-bottom:thin solid #DCE2E6; */
+    }
+    .userData{ 
+        display: flex;
+        flex: 3 0 auto ;
+
+    }
+    .title{ 
+        display: flex;
+        flex: 2 0 auto;
+        font-size: 2rem;
+        font-style: none;
+        margin: auto auto;
+    }
+    .searchbar{ 
+        display: flex;
+        flex: 1 0 auto;
+        position: relative;
+        padding-right: -10%;
+    }
+
+</style>
+
---
+++ components/UserInfo2.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <span class="reg">
+            <div id="nameandfamily">
+                 <!-- {{username}} -->
+                 maryam ebi
+            </div>
+            <div id = "basket">
+                سبد خرید
+                <a id="basketimg"></a>
+            </div>
+            <div id = "order">
+                پیگیری سفارش
+                <a id="orderimg"></a>
+            </div>
+        </span>
+        
+    </div>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style scoped>
+    @font-face {
+        font-family: myFirstFont;
+        src: url(~/assets/font/Shabnam-light.woff);
+    }
+    .reg {
+        flex: 3 0 auto;
+	    display: flex;
+        float: right; 
+        margin-right: 50px; 
+        font-family: myFirstFont       
+    }
+    #basket{
+        flex : 1 0 auto;
+    }
+    #basketimg{
+        background-image: url('~/assets/pic/magnifying-glass1.png');
+        background-repeat: no-repeat;
+        /* background-position: left 7px center; */
+        width: 40%;
+        height: 60%;
+        border-style: solid;
+        border-color:  rgb(112,112,112);
+    }
+    #order{
+        flex :1 0 auto;
+    }
+    #ordertimg{
+        background-image: url('~/assets/pic/magnifying-glass2.png');
+        background-repeat: no-repeat;
+        /* background-position: left 7px center; */
+        width: 40%;
+        height: 60%;
+        border-style: solid;
+        border-color:  rgb(112,112,112);
+    }
+    #nameandfamily{
+        flex :1 0 auto;  
+    }
+
+   
+</style>
